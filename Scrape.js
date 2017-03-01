c230a0c65481e473c22121ae87681a27308c5550
CKIWilkinson <ckiwilkinson@gmail.com>
2017-03-01 12:09:05 +0000

    Update Scrape.js
    Untested, needs to be tested before committing to master.

--- Scrape.js
+++ Scrape.js
@@ -16,22 +16,38 @@ request (address, function (error, response, html) {
 		var arrEmails = knwlInstance.get("emails");
 		console.log("Email addresses found:");
 		//Outputs all found email addresses
-		var arrEmailAddresses = new Array;		//Stores the addresses in an array for future manipulation
+		var arrOutputs = new Array;		//Stores the addresses in an array for future manipulation
 		for (var i = 0; i < arrEmailAdresses.length; i++) {
 			var boolDuplicate = false
 			//Prevents duplicate email addresses from being output
 			for (var z = (i - 1); z > -1; z--) {
-				if (arrEmails[i].address == arrEmails[z].address) {
+				if (arrEmails[0, i].address == arrEmails[0, z].address) {
 					boolDuplicate = true
 				}
 
 			}			
 			if (boolDuplicate == false) {
 				console.log(arrEmails[i].address);
-				arrEmailAddresses.push(emails[i].address);
+				arrOutputs[0].push(arrEmails[i].address);
 			}
 		}
-		
+		console.log("Phone numbers found:");
+		var arrPhoneNumbers = knwlInstance.get("internationalPhones");
+		for (var i = 0; i < arrPhoneNumbers.length; i++) {
+			var boolDuplicate = false
+			//Prevents duplicate phone numbers from being output
+			for (var z = (i - 1); z > -1; z--) {
+				if (arrPhoneNumbers[1, i].address == arrPhoneNumbers[1, z].address) {
+					boolDuplicate = true
+				}
+
+			}			
+			if (boolDuplicate == false) {
+				console.log(arrPhoneNumbers[i].address);
+				arrOutputs[1].push(arrPhoneNumbers[i].address);
+			}
+		}
 	}
+	
 })
 
